35079dd7319c3d63b14749ca55bf3f3e7fa98e29
Craig Price <craigprice00@gmail.com>
2023-11-14 14:22:09 -0700

    Got the database working in the app.py

--- testApplication/app.py
+++ testApplication/app.py
@@ -1,22 +1,60 @@
-from flask import Flask, render_template, request
-import json
+import sqlite3
+import hashlib
+
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    returnDict = {'response': "Make A request Above to get the response"}
-    return render_template("index.html", data=returnDict)
+def connect():
+    conn = sqlite3.connect(':memory:', check_same_thread=False)
+    c = conn.cursor()
+    c.execute("CREATE TABLE users (username TEXT, password TEXT, rank TEXT)")
+    c.execute("INSERT INTO users VALUES ('admin', 'e1568c571e684e0fb1724da85d215dc0', 'admin')")
+    c.execute("INSERT INTO users VALUES ('bob', '2b903105b59299c12d6c1e2ac8016941', 'user')")
+    c.execute("INSERT INTO users VALUES ('alice', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'moderator')")
+
+    c.execute("CREATE TABLE SSN(user_id INTEGER, number TEXT)")
+    c.execute("INSERT INTO SSN VALUES (1, '480-62-10043')")
+    c.execute("INSERT INTO SSN VALUES (2, '690-10-6233')")
+    c.execute("INSERT INTO SSN VALUES (3, '401-09-1516')")
+
+    conn.commit()
+    return conn
 
 
-@app.route('/request', methods=['POST'])
-def requestFunction():
-    requestString = "The request you made was: " + request.form['requestValue']
-    resultString = "The result of that request was: (Nothing Because I don't have a database setup)"
-    returnDict = {'response': requestString, 'result': resultString}
-    return render_template("index.html", data=returnDict)
+CONNECTION = connect()
+
+@app.route("/")
+def home():
+    return render_template("index.html", data="")
+
+@app.route("/login")
+def login():
+    username = request.args.get('username', '')
+    password = request.args.get('password', '')
+    md5 = hashlib.new('md5', password.encode('utf-8'))
+    password = md5.hexdigest()
+    c = CONNECTION.cursor()
+    c.execute("SELECT * FROM users WHERE username = ? and password = ?", (username, password))
+    data = c.fetchone()
+    if data is None:
+        return 'Incorrect username and password.'
+    else:
+        return 'Welcome %s! Your rank is %s.' % (username, data[2])
+
+
+
+@app.route("/users")
+def list_users():
+    rank = request.args.get('rank', '')
+    if rank == 'admin':
+        return "Can't list admins!"
+    c = CONNECTION.cursor()
+    c.execute("SELECT username, rank FROM users WHERE rank = '{0}'".format(rank))
+    data = c.fetchall()
+    return str(data)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
